81fb44dc5718200b4ed815800955b4bdc1cfa567
kpashindla29 <kishore.pashindla@gmail.com>
2023-03-28 10:19:47 +0100

    Message updatedgit add .

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 	@GetMapping("/")
 	public String hello() {
 
-		return "Welcome to Spring Boot App running on AWS EC2 instance!!! Another test!!!";
+		return "Welcome to Spring Boot App running on AWS EC2 instance!!! Another test!!! Good to go!!";
 
 	}
 
